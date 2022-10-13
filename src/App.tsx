3065248e602dc021e39853496c45803d6756b67a
Ricardo Ruiz <ricardo.ruiz@grupoboticario.com.br>
2022-10-13 17:07:22 -0300

    chore: adding a11y storybook addon

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       <h1 className="text-2xl text-black font-bold">Ignite Lab - DS</h1>
       <button className='px-4 py-2 font-medium rounded text-white bg-cyan-500 hover:bg-cyan-300'>Teste</button>
-      <p>Parei no minuto 1:32:10 do vídeo</p>
+      <p>Parei no minuto 29:10 do terceiro vídeo</p>
     </div>
   )
 }
